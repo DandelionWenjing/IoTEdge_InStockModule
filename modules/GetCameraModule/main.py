429be4063a3d61177451ca1bc9db904c5703759f
Wenjing Zhao <wenzhao@microsoft.com>
2018-10-31 11:21:26 +0800

    first commit

---
+++ modules/GetCameraModule/main.py
@@ -0,0 +1,161 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+import os
+import random
+import time
+import sys
+import iothub_client
+import json
+# pylint: disable=E0611
+from iothub_client import IoTHubModuleClient, IoTHubClientError, IoTHubTransportProvider
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError
+
+import requests
+import traceback
+import base64
+import json
+import uuid
+import importlib
+from PIL import Image
+from io import BytesIO
+import numpy as np
+
+# os.system("./img.jpg")
+# messageTimeout - the maximum time in milliseconds until a message times out.
+# The timeout period starts at IoTHubModuleClient.send_event_async.
+# By default, messages do not expire.
+MESSAGE_TIMEOUT = 10000
+
+# global counters
+RECEIVE_CALLBACKS = 0
+SEND_CALLBACKS = 0
+
+# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+PROTOCOL = IoTHubTransportProvider.MQTT
+
+# Callback received when the message that we're forwarding is processed.
+def send_confirmation_callback(message, result, user_context):
+    global SEND_CALLBACKS
+    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
+    map_properties = message.properties()
+    key_value_pair = map_properties.get_internals()
+    print ( "    Properties: %s" % key_value_pair )
+    SEND_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
+
+
+# receive_message_callback is invoked when an incoming message arrives on the specified 
+# input queue (in the case of this sample, "input1").  Because this is a filter module, 
+# we will forward this message onto the "output1" queue.
+def receive_message_callback(message, hubManager):
+    global RECEIVE_CALLBACKS
+    message_buffer = message.get_bytearray()
+    size = len(message_buffer)
+    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
+    map_properties = message.properties()
+    key_value_pair = map_properties.get_internals()
+    print ( "    Properties: %s" % key_value_pair )
+    RECEIVE_CALLBACKS += 1
+    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
+    hubManager.forward_event_to_output("output1", message, 0)
+    return IoTHubMessageDispositionResult.ACCEPTED
+
+def get_image_data(image_url):
+    
+    img_data = requests.get(image_url).content
+    mkdir('../image')
+    with open('../image/img.jpg', 'wb') as handler:
+        handler.write(img_data)
+        return True
+    return False
+           
+    #img_data = requests.get(image_url).content
+    #return(img_data)
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.mkdir(path)
+        print("new folder created.")
+    else:
+        print("the folder is exist.")
+
+def get_image(hub_manager):
+    index=1
+    while(True):
+        try:
+            print("give url address")
+            img_url = "http://retaildevtest.azurewebsites.net/imageget/api/values"
+            if(not is_blank(img_url)):
+                getResponse = get_image_data(img_url)
+                if(getResponse):
+                    print("load image data success")
+                    response = True
+                else:
+                    response = False
+                msg = IoTHubMessage("{{\"response\":\"{0}\", \"messageId\":{1} }}".format(response, index))
+                #payload = []
+                #payload.append("{}".format(base64.b64encode(img_data)))
+                #req_str = json.dumps(payload)
+                #msg = IoTHubMessage("{{\"img\":\"{0}\", \"messageId\":{1} }}".format(req_str, index))
+                # msg = IoTHubMessage("{{\"messageId\":{0} }}".format(index))
+                hub_manager.forward_event_to_output("output1", msg, 0)
+                #return req_str
+            else:
+                #wrong_id = "wrong"
+                msg = IoTHubMessage("{{\"messageId\":{0} }}".format(index))
+                print("can't get the image from image url")
+        except:
+            print("Un expected Error:", traceback.print_exc())
+        time.sleep(5)
+        index=index+1
+
+def is_blank(input):
+    return not (input and input.strip())
+
+class HubManager(object):
+
+    def __init__(
+            self,
+            protocol=IoTHubTransportProvider.MQTT):
+        self.client_protocol = protocol
+        self.client = IoTHubModuleClient()
+        self.client.create_from_environment(protocol)
+
+        # set the time until a message times out
+        self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
+        
+        # sets the callback when a message arrives on "input1" queue.  Messages sent to 
+        # other inputs or to the default will be silently discarded.
+        # self.client.set_message_callback("input1", receive_message_callback, self)
+
+    # Forwards the message received onto the next stage in the process.
+    def forward_event_to_output(self, outputQueueName, event, send_context):
+        self.client.send_event_async(
+            outputQueueName, event, send_confirmation_callback, send_context)
+
+def main(protocol):
+    try:
+        print ( "\nPython %s\n" % sys.version )
+        print ( "IoT Hub Client for Python" )
+        print (os.getcwd())
+
+        hub_manager = HubManager(protocol)
+
+        get_image(hub_manager)
+
+
+        print ( "Starting the IoT Hub Python sample using protocol %s..." % hub_manager.client_protocol )
+        print ( "The sample is now waiting for messages and will indefinitely.  Press Ctrl-C to exit. ")
+
+        while True:
+            time.sleep(1)
+
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubModuleClient sample stopped" )
+
+if __name__ == '__main__':
+    main(PROTOCOL)
